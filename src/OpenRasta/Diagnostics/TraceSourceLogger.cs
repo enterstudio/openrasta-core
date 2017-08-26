e4ba933513e1adad4d1970662126d2da0ae614fe
Greg Shackles <gshackles@gmail.com>
2017-08-26 12:19:26 -0400

    get core project building

--- src/OpenRasta/Diagnostics/TraceSourceLogger.cs
+++ src/OpenRasta/Diagnostics/TraceSourceLogger.cs
@@ -1,22 +1,14 @@
-#region License
-/* Authors:
- *      Sebastien Lambla (seb@serialseb.com)
- * Copyright:
- *      (C) 2007-2009 Caffeine IT & naughtyProd Ltd (http://www.caffeine-it.com)
- * License:
- *      This file is distributed under the terms of the MIT License found at the end of this file.
- */
-#endregion
-
 using System;
 using System.Diagnostics;
+using System.Linq;
+using OpenRasta.DI;
 
 namespace OpenRasta.Diagnostics
 {
     public class TraceSourceLogger<T> : TraceSourceLogger, ILogger<T> where T : ILogSource
     {
         public TraceSourceLogger()
-            : base(new TraceSource(LogSource<T>.Category))
+          : base(new TraceSource(LogSource<T>.Category))
         {
         }
     }
@@ -24,35 +16,44 @@ namespace OpenRasta.Diagnostics
     public class TraceSourceLogger : ILogger
     {
         readonly TraceSource _source;
+        static readonly TraceSource DefaultTraceSource = new TraceSource("openrasta");
 
-        public TraceSourceLogger() : this(new TraceSource("openrasta"))
+        public static readonly ILogger Instance =
+          DependencyManager.Current?.Resolve<ILogger>() ?? new TraceSourceLogger();
+
+        public TraceSourceLogger() : this(DefaultTraceSource)
         {
         }
 
         public TraceSourceLogger(TraceSource source)
         {
             _source = source;
-            _source.Listeners.Remove("Default");
 
-            var listener = new DebuggerLoggingTraceListener
+            if (Debugger.IsLogging() && Debug.Listeners.Count == 0)
             {
-                Name = "OpenRasta", 
-                TraceOutputOptions =
+                var listener = new DebuggerLoggingTraceListener
+                {
+                    Name = "OpenRasta",
+                    Filter = Trace.Listeners.Count == 0
+                    ? new EventTypeFilter(SourceLevels.All)
+                    : new EventTypeFilter(SourceLevels.Verbose),
+                    TraceOutputOptions =
                     TraceOptions.DateTime | TraceOptions.ThreadId |
                     TraceOptions.LogicalOperationStack
-            };
+                };
 
-            _source.Listeners.Add(listener);
-
+                _source.Listeners.Add(listener);
+            }
             _source.Switch = new SourceSwitch("OpenRasta", "All");
         }
 
         public IDisposable Operation(object source, string name)
         {
-            _source.TraceData(TraceEventType.Start, 1, "Entering {0}: {1}".With(source.GetType().Name, name));
+            var msg = $"{source.GetType().Name} ({name})";
+            _source.TraceData(TraceEventType.Start, 1, msg);
             Trace.CorrelationManager.StartLogicalOperation(source.GetType().Name);
 
-            return new OperationCookie { Initiator = source, Source = _source };
+            return new OperationCookie { Initiator = source, Source = _source, Message = msg };
         }
 
         public void WriteDebug(string message, params object[] format)
@@ -70,7 +71,7 @@ namespace OpenRasta.Diagnostics
             if (e == null)
                 return;
             WriteError("An error of type {0} has been thrown", e.GetType());
-            foreach (string line in e.ToString().Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries))
+            foreach (var line in e.ToString().Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries))
                 WriteError(line);
         }
 
@@ -88,31 +89,13 @@ namespace OpenRasta.Diagnostics
         {
             public object Initiator { get; set; }
             public TraceSource Source { get; set; }
+            public string Message { get; set; }
 
             public void Dispose()
             {
                 Trace.CorrelationManager.StopLogicalOperation();
-                Source.TraceData(TraceEventType.Stop, 1, "Exiting {0}".With(Initiator.GetType().Name));
+                Source.TraceData(TraceEventType.Stop, 1, $"Exiting {Initiator.GetType().Name}: {Message}");
             }
         }
     }
 }
-
-#region Full license
-// Permission is hereby granted, free of charge, to any person obtaining
-// a copy of this software and associated documentation files (the
-// "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Software, and to
-// permit persons to whom the Software is furnished to do so, subject to
-// the following conditions:
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#endregion
